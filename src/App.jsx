09e13cc220059ada7b0416a52d63002d2b3587bd
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-09 09:23:21 +1000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-// import "./App.css";
+// import { useState } from "react";
+// import logo from "./logo.svg";
 import { useStore } from "./store";
-
 import KeyboardIntercept from "./KeyboardIntercept";
 import RenderRow from "./components/RenderRow";
-
-// need better naming
-
-// function Controls() {
-//   const increasePopulation = useStore((state) => state.increasePopulation);
-//   return <button onClick={increasePopulation}>one up</button>;
-// }
 
 function App() {
   const wordList = useStore((state) => state.wordList);
 
   return (
     <div>
-      {/* <RenderRow word="TOMMY"></RenderRow>
-      <RenderRow word="TOM"></RenderRow>
-      <RenderRow word=""></RenderRow> */}
-
       {wordList.map((word, i) => (
         <RenderRow key={i} word={word}></RenderRow>
       ))}
-
       <KeyboardIntercept></KeyboardIntercept>
     </div>
   );
