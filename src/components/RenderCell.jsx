b31fed3731ab9b80962c99af523a14cf9a21033f
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-09 09:57:48 +1000

    .

--- src/components/RenderCell.jsx
+++ src/components/RenderCell.jsx
@@ -1,3 +1,14 @@
 export default function RenderCell({ char }) {
-  return <div className="border p-5 m-1 w-16 h-16 text-center">{char}</div>;
+  const backgroundColor = calcBackgroundColor(char, 0, 0, -1, "HELLO");
+
+  // console.log("backgroundColor", backgroundColor);
+
+  // use lib classnames or clsx... ???
+  const className = `border p-5 m-1 w-16 h-16 text-center ${backgroundColor}`;
+
+  return <div className={className}>{char}</div>;
+}
+
+function calcBackgroundColor(char, row, col, cursorRow, solution) {
+  return "bg-indigo-500";
 }
