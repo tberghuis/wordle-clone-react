396aee3ff69616327689afe9e0c98a13a34e36a2
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-08 14:23:47 +1000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useStore } from "./store";
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+function BearCounter() {
+  const bears = useStore((state) => state.bears);
+  return <h1>{bears} around here ...</h1>;
 }
 
-export default App
+function Controls() {
+  const increasePopulation = useStore((state) => state.increasePopulation);
+  return <button onClick={increasePopulation}>one up</button>;
+}
+
+function App() {
+  return (
+    <div>
+      hello world
+      <BearCounter></BearCounter>
+      <Controls></Controls>
+    </div>
+  );
+}
+
+export default App;
