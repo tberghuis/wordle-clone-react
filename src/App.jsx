2ded2fd544ea42fa480d974e46a509040b9267a0
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-09 14:41:33 +1000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-// import { useState } from "react";
-// import logo from "./logo.svg";
-import { useStore } from "./store";
+import { useContext } from "react";
 import KeyboardIntercept from "./KeyboardIntercept";
 import RenderRow from "./components/RenderRow";
 import SnackbarDemo from "./tmp/SnackbarDemo";
 import NewGame from "./tmp/NewGame";
 import ContextDemo from "./tmp/ContextDemo";
+import StateContextProvider, {
+  StateContext,
+} from "./context/StateContextProvider";
 
 function App() {
-  const wordList = useStore((state) => state.wordList);
+  // const wordList = useStore((state) => state.wordList);
+
+  return (
+    <StateContextProvider>
+      <Content></Content>
+    </StateContextProvider>
+  );
+}
+
+export default App;
+
+function Content() {
+  const { state } = useContext(StateContext);
+  const wordList = state.wordList;
 
   return (
     <div>
@@ -23,5 +37,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
