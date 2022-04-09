3361acc542091afc1f66ea0e41452ee251053c10
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-09 14:16:29 +1000

    .

---
+++ src/context/StateContextProvider.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import { TOP_WORDS } from "./const/top_words";
+
+export const StateContext = createContext();
+
+const StateContextProvider = (props) => {
+
+  const [wordList, setWordList] = useState(newWordList());
+  const [cursorRow, setCursorRow] = useState(0);
+  const [solution, setSolution] = useState(randomWord());
+
+  return (
+    <StateContext.Provider value={{ state: { wordList, cursorRow, solution } }}>
+      {props.children}
+    </StateContext.Provider>
+  );
+};
+
+export default StateContextProvider;
+
+function randomWord() {
+  return TOP_WORDS[Math.floor(Math.random() * TOP_WORDS.length)];
+}
+
+function newWordList() {
+  return new Array(6).fill("");
+}
