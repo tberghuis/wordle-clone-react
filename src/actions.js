ec70668b525416ba19faae1fc97d88c8fc8cc572
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-09 17:52:02 +1000

    .

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,20 @@
-
+import { useWordleStore } from "./wordleStore";
 
 export function addLetter(letter) {
-  console.log("addLetter", letter);
+  const { wordList, cursorRow } = useWordleStore.getState();
+  console.log("addLetter", wordList);
+  console.log("addLetter", cursorRow);
+
+  const word = wordList[cursorRow];
+  if (word.length === 5) {
+    return;
+  }
+
+  wordList[cursorRow] = `${wordList[cursorRow]}${letter}`;
+
+  useWordleStore.setState({ wordList: [...wordList] });
+}
+
+export function removeLetter() {
+  console.log("removeLetter");
 }
