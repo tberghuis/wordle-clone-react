f9b0abb469c5b03e066ee315e0f2b93afd0ab79b
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-09 11:16:02 +1000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import { useStore } from "./store";
 import KeyboardIntercept from "./KeyboardIntercept";
 import RenderRow from "./components/RenderRow";
+import SnackbarDemo from "./tmp/SnackbarDemo";
 
 function App() {
   const wordList = useStore((state) => state.wordList);
@@ -13,6 +14,8 @@ function App() {
         <RenderRow key={i} word={word} row={i}></RenderRow>
       ))}
       <KeyboardIntercept></KeyboardIntercept>
+
+      <SnackbarDemo></SnackbarDemo>
     </div>
   );
 }
---
+++ src/tmp/SnackbarDemo.jsx
@@ -0,0 +1,3 @@
+export default function SnackbarDemo() {
+  return <div>hello snackbar</div>;
+}
