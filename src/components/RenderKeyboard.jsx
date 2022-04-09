85f9ddc188c29d83212b86e51de0a12f53bcddb5
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-10 06:40:23 +1000

    .

--- src/components/RenderKeyboard.jsx
+++ src/components/RenderKeyboard.jsx
@@ -1,14 +1,22 @@
+import clsx from "clsx";
+
 // doitwrong
 export default function RenderKeyboard() {
   const row1 = "QWERTYUIOP".split("");
   const row2 = "ASDFGHJKL".split("");
   const row3 = "ZXCVBNM".split("");
 
+  const rowStyle = "flex flex-row justify-center";
+  const keyStyle =
+    "flex flex-col justify-center border m-1 w-10 h-16 text-center ";
+
   return (
     <div>
-      <div className="flex flex-row">
+      <div className={rowStyle}>
         {row1.map((k, i) => (
-          <div key={i}>{k}</div>
+          <div key={i} className={keyStyle}>
+            <p>{k}</p>
+          </div>
         ))}
       </div>
     </div>
